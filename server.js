d341b1d05dab8412a4c1e096c7c1405b5129b18a
aa-arqamawan <arqamawan2233@gmail.com>
2022-02-25 10:55:04 +0500

    add today

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+ '/contact.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+
+        service: 'gmail',
+        auth:{
+            user: 'arqamawan4060@gmail.com',
+            pass: 'arqamawan123'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'arqamawan4060@gmail.com',
+        subject: req.body.subject,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send(error);
+        }else{
+            console.log('Email Sent: ' + info.response);
+            res.send('success');
+        }
+    });
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server running on port ${PORT}')
+})
